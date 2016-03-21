e5c0ac0e791287aceeef170ca7b1f5ab0db3c718
Anthony Torfs <torfs.anthony@hotmail.com>
2016-03-21 10:35:15 +0100

    First save

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+
+  get 'contact', to: 'pages#contact'
+
+  get 'about', to: 'pages#about'
+
+  get 'home', to: 'pages#home'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
